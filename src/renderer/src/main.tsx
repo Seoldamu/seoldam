fe06d8b70cc94dd76e6834d1eb3523c96316bbd9
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-30 12:06:04 +0900

    feat(#6): styles 파일 위치 변경 및 모듈로 내보내기

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App'
-import GlobalStyle from './shared/styles/global.css'
+import { GlobalStyle } from './styles'
 import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
