b3235d6e4306d5808299af47907305bad43ba4f6
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-30 13:36:41 +0900

    feat(#5): design 폴더 안에 icons, styles, assets 위치
    프로젝트의 규모가 크지 않기 때문에 이러한 형태로 묶었습니다.

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App'
-import { GlobalStyle } from './styles'
+import { GlobalStyle } from './design/styles'
 import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
