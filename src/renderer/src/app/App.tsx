0793dd7fc51a8dcb6a8122ed13a753861cda63e9
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-30 15:25:25 +0900

    feat(#5): 임시페이지 라우팅

--- src/renderer/src/app/App.tsx
+++ src/renderer/src/app/App.tsx
@@ -1,10 +1,18 @@
-import Home from '@renderer/pages/Home'
+import { Home, Editor, Plot, Lab, Memo, Character } from '@renderer/pages'
 import { Route, Routes } from 'react-router-dom'
+import AppLayout from './AppLayout'
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/editor" element={<Editor />} />
+        <Route path="/plot" element={<Plot />} />
+        <Route path="/lab" element={<Lab />} />
+        <Route path="/memo" element={<Memo />} />
+        <Route path="/character" element={<Character />} />
+      </Route>
     </Routes>
   )
 }
