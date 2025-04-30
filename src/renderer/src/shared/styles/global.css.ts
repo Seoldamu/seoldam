14bc5799536ff77dd1c1d2afbd223ed348596bd1
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-30 10:05:53 +0900

    refactor(#1): 미디워 쿼리 중단점 리펙토링

--- src/renderer/src/shared/styles/global.css.ts
+++ src/renderer/src/shared/styles/global.css.ts
@@ -3,35 +3,12 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
 @import url('https://cdn.jsdelivr.net/gh/fonts-archive/Paperlogy/Paperlogy.css');
 
-  :root {
-    font-size: 100%;
-  }
-
-  @media screen and (min-width: 1026px) and (max-width: 1440px) {
-    :root {
-      font-size: 90%;
-    }
-  }
-  @media screen and (min-width: 1025px) and (max-width: 1026px) {
-    :root {
-      font-size: 80%;
-    }
-  }
-  @media screen and (min-width: 540px) and (max-width: 768px) {
-    :root {
-      font-size: 70%;
-    }
-  }
-  @media screen and (min-width: 300px) and (max-width: 540px) {
-    :root {
-      font-size: 60%;
-    }
-  }
-  @media screen and (max-width: 300px) {
-    :root {
-      font-size: 50%;
-    }
-  }
+  :root { font-size: 100%; }
+  @media (max-width: 1440px) { :root { font-size: 90%; } }
+  @media (max-width: 1024px) { :root { font-size: 80%; } }
+  @media (max-width: 768px)  { :root { font-size: 70%; } }
+  @media (max-width: 520px)  { :root { font-size: 60%; } }
+  @media (max-width: 300px)  { :root { font-size: 50%; } }
 
   * {
     margin: 0;
