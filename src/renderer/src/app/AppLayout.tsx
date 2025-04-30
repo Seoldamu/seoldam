c98b6908ee17b8f428bbf840e33ce71aebbbd8d9
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-30 15:12:33 +0900

    feat(#5): Sidebar 포함한 AppLayout 컴포넌트 추가

---
+++ src/renderer/src/app/AppLayout.tsx
@@ -0,0 +1,29 @@
+import { Sidebar } from '@renderer/components/common'
+import { flex } from '@renderer/utils'
+import { Outlet } from 'react-router-dom'
+import { styled } from 'styled-components'
+
+const AppLayout = () => {
+  return (
+    <StyledAppLayout>
+      <Sidebar />
+      <Section>
+        <Outlet />
+      </Section>
+    </StyledAppLayout>
+  )
+}
+
+export default AppLayout
+
+const StyledAppLayout = styled.div`
+  ${flex({ flexDirection: 'row' })}
+  width: 100%;
+  height: 100vh;
+`
+
+const Section = styled.section`
+  flex: 1;
+  min-width: fit-content;
+  overflow: auto;
+`
