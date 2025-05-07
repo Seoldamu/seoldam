946f6774d6fbd9af1b864fe1a42021826ec60013
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-07 13:50:37 +0900

    feat(#9): 정적인 그래프 구현
    데이터가 정적인 그래프를 구현. 즉, 퍼블리싱 했습니다.

--- src/renderer/src/components/home/Chart/Chart.tsx
+++ src/renderer/src/components/home/Chart/Chart.tsx
@@ -31,7 +31,7 @@ const Chart = () => {
     legend: {
       data: ['이번 달 쓴 글자 수', '저번 달 쓴 글자 수'],
       top: 0,
-      right: 30,
+      right: 0,
       orient: 'vertical',
       itemGap: 4,
       icon: 'circle',
@@ -45,6 +45,123 @@ const Chart = () => {
         letterSpacing: 0.12,
         color: `${color.G900}`
       }
+    },
+    series: [
+      {
+        name: '이번 달 쓴 글자 수',
+        type: 'line',
+        data: [3200, 3000, 3200, 3000, 3200, 3000, 3200, 3000], // 후에 동적인 값
+        smooth: true,
+        showSymbol: false,
+        emphasis: {
+          focus: 'series',
+          showSymbol: true,
+          itemStyle: {
+            color: '#344BFD',
+            borderColor: `${color.G0}`,
+            borderWidth: 3
+          },
+          label: {
+            show: true,
+            position: 'top',
+            fontFamily: 'Paperlogy',
+            fontWeight: 700,
+            fontSize: 13,
+            lineHeight: 13,
+            letterSpacing: 0.13,
+            color: '#001DFF'
+          }
+        },
+        itemStyle: {
+          color: '#001DFF'
+        },
+        lineStyle: {
+          color: '#001DFF',
+          width: 1.32
+        },
+        areaStyle: {
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+              { offset: 0, color: '#ADB7F9' },
+              { offset: 1, color: 'rgba(177,185,248,0)' }
+            ]
+          }
+        }
+      },
+      {
+        name: '저번 달 쓴 글자 수',
+        type: 'line',
+        data: [2100, 2500, 2400, 2100, 2500, 2400, 2100, 2500, 2400], // 후에 동적인 값
+        smooth: true,
+        showSymbol: false,
+        emphasis: {
+          focus: 'series',
+          showSymbol: true,
+          itemStyle: {
+            color: '#F00000',
+            borderColor: '#fff',
+            borderWidth: 3
+          },
+          label: {
+            show: true,
+            position: 'top',
+            fontFamily: 'Paperlogy',
+            fontWeight: 700,
+            fontSize: 13,
+            lineHeight: 13,
+            letterSpacing: 0.13,
+            color: '#F00000'
+          }
+        },
+        itemStyle: {
+          color: '#F00000'
+        },
+        lineStyle: {
+          color: '#F00000',
+          width: 1.32
+        },
+        areaStyle: {
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+              { offset: 0, color: '#F4A79D' },
+              { offset: 1, color: 'rgba(244,167,157,0)' }
+            ]
+          }
+        }
+      }
+    ],
+    grid: {
+      left: 0,
+      right: 0,
+      top: `76px`,
+      bottom: 0,
+      containLabel: true
+    },
+    xAxis: {
+      type: 'category',
+      data: ['10', '11', '12', '13', '14', '15', '1', '123', '123123'], // 날짜
+      axisLabel: {
+        fontFamily: 'Paperlogy',
+        fontWeight: 700,
+        fontSize: 13,
+        color: `${color.G600}`,
+        lineHeight: 13,
+        letterSpacing: 0.13,
+        interval: 0
+      }
+    },
+    yAxis: {
+      type: 'value'
     }
   }
 
