757be53039c54bcd25d1040b8772c20a6147bca7
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-27 17:12:56 +0900

    feat(#11): Button 컴포넌트 추가
    디자인 시스템에 정의된 DEFAULT와 DISABLED 상태의 Button 컴포넌트를 추가했습니다.

---
+++ src/renderer/src/design/ui/Button/Button.tsx
@@ -0,0 +1,51 @@
+import { color, font } from '@renderer/design/styles'
+import { flex } from '@renderer/utils'
+import { ButtonHTMLAttributes, ReactNode } from 'react'
+import styled, { CSSProperties } from 'styled-components'
+
+type ButtonProperty = 'DEFAULT' | 'DISABLED'
+
+type ButtonSize = 'SMALL'
+
+type Props = {
+  children: ReactNode
+  property: ButtonProperty
+  size?: ButtonSize
+  width?: CSSProperties['width']
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button = ({ onClick, children, property, width, style, disabled }: Props) => {
+  return (
+    <StyledButton
+      property={property}
+      style={{ width, ...style }}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </StyledButton>
+  )
+}
+
+export default Button
+
+const StyledButton = styled.button<{ property: ButtonProperty }>`
+  ${flex({ alignItems: 'center', justifyContent: 'center' })}
+  padding: 6px 14px;
+  word-break: keep-all;
+  box-sizing: border-box;
+  gap: 8px;
+
+  border-radius: 6px;
+  ${font.L2}
+  ${({ property }) =>
+    property === 'DEFAULT'
+      ? `
+        background: ${color.primary};
+        color: ${color.G0};
+      `
+      : `
+        background: ${color.G20};
+        color: ${color.G600};
+      `}
+`
