3c4d9214c296aa310f17b4a099b8c915897b732f
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-28 08:20:09 +0900

    feat(#14): 미니드롭다운 추가

--- src/renderer/src/design/ui/Dropdown/MiniDropdown.tsx
+++ src/renderer/src/design/ui/Dropdown/MiniDropdown.tsx
@@ -1,6 +1,10 @@
+import { Text } from '@renderer/components/common'
+import { IconFolder } from '@renderer/design/icons'
 import { color } from '@renderer/design/styles'
+import { useBoolean, useOutsideClick } from '@renderer/hooks'
 import { flex } from '@renderer/utils'
-import { styled } from 'styled-components'
+import { ReactNode } from 'react'
+import { CSSProperties, styled } from 'styled-components'
 
 interface Data {
   label: string
@@ -8,41 +12,102 @@ interface Data {
 }
 
 interface Props {
+  icon?: ReactNode
   name: string
+  label: string
   value: string
   data: Data[]
-  onSelect: (name: string, value: string) => void
+  onChange: (value: Data, name: string) => void
+  placeholder?: string
+  width?: CSSProperties['width']
+  disabled?: boolean
 }
 
-const MiniDropdown = ({ name, data, onSelect }: Props) => {
-  const handleSelect = (value: string) => {
-    onSelect(value, name)
+const MiniDropdown = ({
+  icon = <IconFolder width={20} height={20} />,
+  name,
+  label,
+  data,
+  onChange,
+  placeholder,
+  width = '124px',
+  disabled = false
+}: Props) => {
+  const { value: isOpen, setFalse: closeDropdown, toggle: handleToggleButtonClick } = useBoolean()
+  const dropdownRef = useOutsideClick<HTMLDivElement>(closeDropdown)
+
+  const handleDropdownItemClick = (data: Data) => {
+    if (!disabled) {
+      onChange(data, name)
+      closeDropdown()
+    }
+  }
+
+  const handleDropdownClick = () => {
+    if (!disabled) {
+      handleToggleButtonClick()
+    }
   }
 
   return (
-    <StyledMiniDropdown>
-      {data.map((item) => (
-        <MiniDropdownItem key={item.value} onClick={() => handleSelect(item.value)}>
-          {item.label}
-        </MiniDropdownItem>
-      ))}
-    </StyledMiniDropdown>
+    <div ref={dropdownRef} style={{ width }}>
+      <StyledMiniDropdown onClick={handleDropdownClick} $isOpen={isOpen}>
+        {icon}
+        <Text fontType="L2" color={color.G700} ellipsis={true}>
+          {label || placeholder}
+        </Text>
+      </StyledMiniDropdown>
+      <MiniDropdownListBox $isOpen={isOpen && !disabled}>
+        <MiniDropdownList>
+          {data?.map((item, index) => {
+            return (
+              <MiniDropdownItem
+                key={`dropdown ${index}`}
+                onClick={() => handleDropdownItemClick(item)}
+              >
+                {item.label}
+              </MiniDropdownItem>
+            )
+          })}
+        </MiniDropdownList>
+      </MiniDropdownListBox>
+    </div>
   )
 }
 
 export default MiniDropdown
 
-const StyledMiniDropdown = styled.div`
-  ${flex({ flexDirection: 'column', alignItems: 'flex-start' })}
-  width: 130px;
-  padding: 4px 0px;
-  gap: 8px;
-
-  border-radius: 4px;
+const StyledMiniDropdown = styled.div<{
+  $isOpen: boolean
+}>`
+  ${flex({ alignItems: 'center' })}
+  width: 100%;
+  gap: 4px;
+  cursor: pointer;
   background: ${color.G0};
 `
 
-const MiniDropdownItem = styled.div`
+const MiniDropdownListBox = styled.div<{ $isOpen: boolean }>`
+  position: relative;
+  top: 100%;
+  left: 24px;
+  display: ${(props) => (props.$isOpen ? 'block' : 'none')};
+`
+
+const MiniDropdownList = styled.div`
+  position: absolute;
+  ${flex({ flexDirection: 'column', alignItems: 'flex-start' })}
+  width: 100%;
+  padding: 4px 0px;
+  gap: 2px;
+  z-index: 1;
+
+  border-radius: 4px;
+  background: ${color.G0};
+  box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.25);
+`
+
+const MiniDropdownItem = styled.button`
   ${flex({ alignItems: 'center' })}
   padding: 5px 4px 5px 6px;
   gap: 8px;
