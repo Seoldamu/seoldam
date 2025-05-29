0409a85be43816545f22a293de3af83395e017f6
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-29 11:15:29 +0900

    feat(#23): OverlayWrapper 컴포넌트 추가
    여러 모달 컴포넌트에서 사용할 가능성이 다분한 오버레이를 추가했습니다.

---
+++ src/renderer/src/components/common/Overlay/OverlayWrapper.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+import { ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+const OverlayWrapper = ({ children }: Props) => {
+  return <StyledOverlayWrapper>{children}</StyledOverlayWrapper>
+}
+
+export default OverlayWrapper
+
+const StyledOverlayWrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  background: rgba(120, 120, 120, 0.5);
+  z-index: 1000;
+`
