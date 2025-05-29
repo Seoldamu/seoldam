3926491389e46d1343a95dafc07ddba0da1c5022
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-29 10:40:24 +0900

    feat(#23): Textarea 컴포넌트 추가
    여러 줄 입력이 필요했기에 추가했습니다.

---
+++ src/renderer/src/components/common/Input/TextArea.tsx
@@ -0,0 +1,65 @@
+import { color, font } from '@renderer/design/styles'
+import { HTMLAttributes } from 'react'
+import { CSSProperties } from 'styled-components'
+import styled from 'styled-components'
+
+type Font = keyof typeof font
+
+interface Props extends HTMLAttributes<HTMLTextAreaElement> {
+  value?: string
+  onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
+  color?: CSSProperties['color']
+  fontType?: Font
+  width?: CSSProperties['width']
+  textAlign?: CSSProperties['textAlign']
+  placeholder?: string
+  rows?: number
+  wrap?: 'soft' | 'hard' | 'off'
+}
+
+const TextArea = ({
+  value,
+  onChange,
+  color,
+  fontType = 'H1',
+  width = 'auto',
+  textAlign = 'left',
+  placeholder = '텍스트를 입력해주세요',
+  rows = 4,
+  wrap = 'soft',
+  ...rest
+}: Props) => {
+  return (
+    <StyledTextArea
+      style={{ color, textAlign, width }}
+      value={value}
+      onChange={onChange}
+      fontType={fontType}
+      rows={rows}
+      wrap={wrap}
+      placeholder={placeholder}
+      {...rest}
+    />
+  )
+}
+
+export default TextArea
+
+const StyledTextArea = styled.textarea<{
+  fontType: Font
+}>`
+  ${({ fontType }) => font[fontType]};
+  resize: none;
+  box-sizing: border-box;
+  outline: none;
+
+  &::placeholder {
+    color: ${color.G50};
+  }
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`
