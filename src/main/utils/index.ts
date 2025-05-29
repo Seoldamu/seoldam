bc4e86c37ae28d942cba0c38067959df1efe2934
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-05-29 20:12:46 +0900

    feat(#23): 메타 데이터 생성 함수 추가

--- src/main/utils/index.ts
+++ src/main/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as generateSeriesId } from './generateSeriesId'
+export { default as createMetaJson } from './createMetaJson'
 export { default as generateUniqueFilename } from './generateUniqueFilename'
