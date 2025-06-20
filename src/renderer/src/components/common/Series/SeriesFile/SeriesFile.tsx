6384878bfe56fe588b9a4ba27457362007d0d2bd
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-06-20 09:48:58 +0900

    feat(#29): 시리즈 파일 삭제 기능 추가

--- src/renderer/src/components/common/Series/SeriesFile/SeriesFile.tsx
+++ src/renderer/src/components/common/Series/SeriesFile/SeriesFile.tsx
@@ -4,19 +4,60 @@ import { styled } from 'styled-components'
 import { Text } from '@renderer/components/common'
 import { color } from '@renderer/design/styles'
 import { TreeNode } from '@renderer/types/series/type'
+import { ContextMenu } from '@renderer/components/common'
+import { useContextMenu, useOutsideClick } from '@renderer/hooks'
+import { useSeriesTreeStore } from '@renderer/stores'
 
 interface Props {
   node: TreeNode
 }
 
 const SeriesFile = ({ node }: Props) => {
+  const { contextMenuVisible, contextMenuPosition, openContextMenu, closeContextMenu } =
+    useContextMenu()
+
+  const contextMenuRef = useOutsideClick<HTMLDivElement>(() => {
+    if (contextMenuVisible) {
+      closeContextMenu()
+    }
+  })
+
+  const contextMenuData = [
+    {
+      label: '파일 삭제',
+      value: 'delete',
+      onClick: async () => {
+        closeContextMenu()
+        const result = await window.api.deleteSeriesTargetPath(node.path)
+        if (result.success) {
+          useSeriesTreeStore.getState().fetchTreeData()
+        }
+      }
+    }
+  ]
+
   return (
-    <StyledSeriesFile>
-      <IconArticle width={24} height={24} />
-      <Text fontType="B2" color={color.G800} ellipsis={true}>
-        {node.name}
-      </Text>
-    </StyledSeriesFile>
+    <>
+      <StyledSeriesFile onContextMenu={openContextMenu}>
+        <IconArticle width={24} height={24} />
+        <Text fontType="B2" color={color.G800} ellipsis={true}>
+          {node.name}
+        </Text>
+      </StyledSeriesFile>
+      {contextMenuVisible && (
+        <div
+          ref={contextMenuRef}
+          style={{
+            position: 'fixed',
+            top: contextMenuPosition.y,
+            left: contextMenuPosition.x,
+            zIndex: 9999
+          }}
+        >
+          <ContextMenu data={contextMenuData} />
+        </div>
+      )}
+    </>
   )
 }
 
