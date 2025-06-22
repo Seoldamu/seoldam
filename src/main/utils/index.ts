004ebf901ce6f182a30792fe178980480377cd65
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-06-23 08:40:55 +0900

    feat(#29): 파일 및 폴더 네이밍 검수 함수 추가

--- src/main/utils/index.ts
+++ src/main/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as generateSeriesId } from './generateSeriesId'
 export { default as createMetaJson } from './createMetaJson'
 export { default as generateUniqueFilename } from './generateUniqueFilename'
+export { default as validatePathName } from './validatePathName'
