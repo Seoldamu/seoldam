c3f9223f6bc3043a91788cbca8033fbf49badbc5
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-07-15 14:06:14 +0900

    feat(#33): ChatInput 확장

--- src/renderer/src/components/common/Input/ChatInput.tsx
+++ src/renderer/src/components/common/Input/ChatInput.tsx
@@ -1,9 +1,11 @@
 import { color, font } from '@renderer/design/styles'
 import { useRef } from 'react'
 import styled from 'styled-components'
+import Column from '../Flex/Column'
+import { flex } from '@renderer/utils'
 
 interface ChatInputProps {
-  onSend: (message: string) => void
+  onSend?: (message: string) => void
 }
 
 const ChatInput = ({ onSend }: ChatInputProps) => {
@@ -12,7 +14,7 @@ const ChatInput = ({ onSend }: ChatInputProps) => {
   const handleSend = () => {
     const text = ref.current?.innerText.trim()
     if (!text) return
-    onSend(text)
+    onSend?.(text)
     if (ref.current) ref.current.innerText = ''
   }
 
@@ -46,13 +48,18 @@ const ChatInput = ({ onSend }: ChatInputProps) => {
 
   return (
     <InputWrapper>
-      <ContentEditable
-        contentEditable
-        ref={ref}
-        onKeyDown={handleKeyDown}
-        onInput={scrollToCaret}
-        data-placeholder="메시지를 입력하세요..."
-      />
+      <Column>
+        <ContentEditable
+          contentEditable
+          ref={ref}
+          onKeyDown={handleKeyDown}
+          onInput={scrollToCaret}
+          data-placeholder="메시지를 입력하세요..."
+        />
+        <ButtonWrapper>
+          {/*TODO: 이성, 글쓰기, 등 AI 모드 적용 버튼과 마우스로 handleSend 버튼 추가 */}
+        </ButtonWrapper>
+      </Column>
     </InputWrapper>
   )
 }
@@ -61,9 +68,9 @@ export default ChatInput
 
 const InputWrapper = styled.div`
   position: relative;
-  width: 667px;
+  width: 100%;
   padding-right: 12px;
-  background: 1px soild ${color.G0};
+  background: ${color.G0};
   border: 1px solid ${color.G50};
   border-radius: 15px;
 
@@ -73,12 +80,12 @@ const InputWrapper = styled.div`
 `
 
 const ContentEditable = styled.div`
-  width: 100%;
+  width: 98%;
   ${font.L2}
   max-height: 10em;
   min-height: 42px;
   overflow-y: auto;
-  padding: 10px 28px 10px 12px;
+  padding: 12px 28px 12px 16px;
 
   border: none;
   outline: none;
@@ -106,4 +113,10 @@ const ContentEditable = styled.div`
     content: attr(data-placeholder);
     color: ${color.G50};
   }
+`
+
+const ButtonWrapper = styled.div`
+  ${flex({ flexDirection: 'row', alignItems: 'center' })}
+  width: 100%;
+  padding: 10px 28px 8px 16px;
 `
