c6d9054f89036f55440413bfbf1a49bd492b0508
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-07-15 14:13:37 +0900

    feat(#33): UserMessage 퍼블리싱

---
+++ src/renderer/src/components/lab/ChatBot/ChatMessage/UserMessage.tsx
@@ -0,0 +1,35 @@
+import { color, font } from '@renderer/design/styles'
+import { styled } from 'styled-components'
+
+interface UserMessageProps {
+  message: string
+}
+
+const UserMessage = ({ message }: UserMessageProps) => {
+  return (
+    <MessageWrapper>
+      <StyledUserMessage>{message}</StyledUserMessage>
+    </MessageWrapper>
+  )
+}
+
+export default UserMessage
+
+const MessageWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 4px 0;
+`
+
+const StyledUserMessage = styled.div`
+  display: inline-block;
+  max-width: 70%;
+  background: ${color.primary};
+  color: ${color.G0};
+  padding: 10px 14px;
+  border-radius: 12px;
+
+  ${font.B1};
+  word-break: break-word;
+  white-space: pre-wrap;
+`
