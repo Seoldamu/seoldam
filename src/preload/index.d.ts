9a0160d11a39817c12f18b3733528d6e5e868de8
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-07-16 00:06:17 +0900

    feat(#34): preload 타입 추가

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,7 +1,29 @@
+import { ElectronAPI } from '@electron-toolkit/preload'
+
 declare global {
   interface Window {
-    api: api
-    electron: electronAPI
+    electron: ElectronAPI
+    api: {
+      ai: {
+        askChatbot: (prompt: string, seriesPath: string | null) => Promise<string>
+      }
+      series: {
+        create: (seriesName: string, seriesImagePath: string) => Promise<any>
+        getList: () => Promise<any[]>
+        getCharCountsList: (date: { year: number; month: number }) => Promise<any[]>
+        getRootDirectory: (seriesPath: string) => Promise<any>
+      }
+      fs: {
+        getDirectory: (dirPath: string) => Promise<any>
+        delete: (targetPath: string) => Promise<any>
+        createFolder: (targetPath: string, name: string) => Promise<any>
+        createFile: (targetPath: string, name: string) => Promise<any>
+        rename: (targetPath: string, name: string) => Promise<any>
+        saveFile: (filePath: string, content: string) => Promise<any>
+        getFileInfo: (filePath: string) => Promise<any>
+        getPathForFile: (file: File) => Promise<string>
+      }
+    }
   }
 }
 
